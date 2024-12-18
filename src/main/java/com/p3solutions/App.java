d186c9541d3fcaa8819f56014c59fa43c97d0ded
clementdicksona <clementdicksona@platform3solutions.com>
2024-12-18 15:03:41 +0530

    first commit

---
+++ src/main/java/com/p3solutions/App.java
@@ -0,0 +1,13 @@
+package com.p3solutions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
